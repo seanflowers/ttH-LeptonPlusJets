96a02aef334915e8f17e6725a408c841f128eb6e
Satoshi Hasegawa <satoshi@fnal.gov>
2015-09-15 08:08:07 -0500

    Now, the crab_script for ttH sample uses not PHYS14 but Spring15 sample.

--- YggdrasilTreeMaker/test/crab3_yggdrasil_treeMaker_ttH_13TeV.py
+++ YggdrasilTreeMaker/test/crab3_yggdrasil_treeMaker_ttH_13TeV.py
@@ -1,7 +1,7 @@
 from WMCore.Configuration import Configuration 
 config = Configuration() 
 config.section_("General") 
-config.General.requestName = 'treeMaker_PHYS14' ## change
+config.General.requestName = 'treeMaker_SPRING15_Sep15' ## change
 
 config.section_("JobType") 
 config.JobType.pluginName = 'Analysis' 
@@ -9,7 +9,7 @@ config.JobType.psetName = 'yggdrasil_treeMaker_cfg.py'
 config.JobType.allowUndistributedCMSSW = False
 
 config.section_("Data") 
-config.Data.inputDataset = '/TTbarH_M-125_13TeV_amcatnlo-pythia8-tauola/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v2/MINIAODSIM' ## change
+config.Data.inputDataset = '/ttHTobb_M125_13TeV_powheg_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM' ## change
 config.Data.inputDBS = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/' 
 config.Data.splitting = 'FileBased' 
 config.Data.unitsPerJob = 1
@@ -18,7 +18,7 @@ config.Data.publication = True
 config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/' 
 config.Data.publishDataName = 'Validation_v1'
 ### change user Space 
-config.Data.outLFNDirBase = '/store/user/lwming/'
+config.Data.outLFNDirBase = '/store/user/satoshi/'
 config.Data.ignoreLocality = True
 
 config.section_("Site") 
