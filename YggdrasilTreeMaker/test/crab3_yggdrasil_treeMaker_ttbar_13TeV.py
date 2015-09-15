f5d7534a435ec485018571cd5d82ef8e281b9758
Satoshi Hasegawa <satoshi@fnal.gov>
2015-09-15 08:11:13 -0500

    One can use a crab_script for the ttbar sample.

---
+++ YggdrasilTreeMaker/test/crab3_yggdrasil_treeMaker_ttbar_13TeV.py
@@ -0,0 +1,25 @@
+from WMCore.Configuration import Configuration 
+config = Configuration() 
+config.section_("General") 
+config.General.requestName = 'treeMaker_SPRING15_Sep15' ## change
+
+config.section_("JobType") 
+config.JobType.pluginName = 'Analysis' 
+config.JobType.psetName = 'yggdrasil_treeMaker_cfg.py' 
+config.JobType.allowUndistributedCMSSW = False
+
+config.section_("Data") 
+config.Data.inputDataset = '/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM' ## change
+config.Data.inputDBS = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/' 
+config.Data.splitting = 'FileBased' 
+config.Data.unitsPerJob = 1
+#config.Data.totalUnits = 100
+config.Data.publication = True 
+config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/' 
+config.Data.publishDataName = 'Validation_v1'
+### change user Space 
+config.Data.outLFNDirBase = '/store/user/satoshi/'
+config.Data.ignoreLocality = True
+
+config.section_("Site") 
+config.Site.storageSite = 'T3_US_FNALLPC'
